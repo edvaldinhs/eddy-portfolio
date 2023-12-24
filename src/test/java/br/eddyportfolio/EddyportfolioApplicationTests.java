2786223e352c94a0227b3fe12b860c6a01368029
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2023-12-24 16:34:07 -0300

    trying to merge

---
+++ src/test/java/br/eddyportfolio/EddyportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.eddyportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EddyportfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
