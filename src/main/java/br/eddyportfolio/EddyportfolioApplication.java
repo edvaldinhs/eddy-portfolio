d8ab6790f0ca3fa66e35c0990e20dbe1579d0921
[edvaldinhs] <[edvaldohenriqueceu@gmail.com]>
2023-12-24 16:26:41 -0300

    first commit

---
+++ src/main/java/br/eddyportfolio/EddyportfolioApplication.java
@@ -0,0 +1,13 @@
+package br.eddyportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddyportfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddyportfolioApplication.class, args);
+	}
+
+}
